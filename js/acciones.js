a9946ddd0b9feb0500f8c462bbd42a56e563c089
guadalupe07112001 <lupita0072001@gmail.com>
2019-05-13 09:20:08 -0500

    3

--- js/acciones.js
+++ js/acciones.js
@@ -1,4 +1,30 @@
 //JavaScript Document
+
+$(document).ready(function (e){
+	document .addEventListener ("deviceready",function(){
+		$('#disp table td').eq(3).text(device.model);
+		$('#disp table td').eq(5).text(device.cordova);
+        $('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
+	},false);//ready device
+	
+});//document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		
+		$('#beep').tap(function(){
+			navigator.notification.beep(1);
+		});//tap beep
+		
+		$('#vibrar').tap(function(){
+			navigator.notification.vibrate(1000);
+		});//tap vibrar
+		
+	},false);//deviceready
+});//ready
+
 $(document).ready(function (e){
 	document.addEventListener ("deviceready",function(){
 		
@@ -24,28 +50,3 @@ $(document).ready(function (e){
 		});
 	},false);
 });
-
-$(document).ready(function(e){
-	document.addEventListener("deviceready",function(){
-		
-		$('#beep').tap(function(){
-			navigator.notification.beep(1);
-		});//tap beep
-		
-		$('#vibrar').tap(function(){
-			navigator.notification.vibrate(1000);
-		});//tap vibrar
-		
-	},false);//deviceready
-});//ready
-
-$(document).ready(function (e){
-	document .addEventListener ("deviceready",function(){
-		$('#disp table td').eq(3).text(device.model);
-		$('#disp table td').eq(5).text(device.cordova);
-        $('#disp table td').eq(7).text(device.platform);
-		$('#disp table td').eq(9).text(device.version);
-		$('#disp table td').eq(11).text(device.uuid);
-	},false);//ready device
-	
-});//document
